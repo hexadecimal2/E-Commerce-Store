9e27f3090520cfb8ddd926ec2f36a6321fc38f51
MbaliPuso <mbalipuso@gmail.com>
2024-07-24 01:32:35 +0200

    provider wrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import ItemView from './pages/ItemView';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Provider>
-          <BrowserRouter>
+      <Provider store={store}> 
+          <BrowserRouter> 
           <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/item-view' element={<ItemView />} />
           </Routes>
         </BrowserRouter>
-    </Provider>
+      </Provider> 
     </>
   );
 }

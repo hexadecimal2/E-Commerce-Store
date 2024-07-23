8628529315e16f6f05a4d698d1f5ce34f6f09909
MbaliPuso <mbalipuso@gmail.com>
2024-07-23 18:43:32 +0200

    import products

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductList from '../components/ProductList';
+
+const Dashboard = () => {
+    return (
+        <>
+        <ProductList />
+        </>
+    );
+};
+
+export default Dashboard;

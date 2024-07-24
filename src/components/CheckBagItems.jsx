ab3e090c180d0d348fc35bf395829b89f198e285
Siya <tonjenisiya@gmail.com>
2024-07-25 01:11:14 +0200

    Few Adjustments - Added Ratings

--- src/components/CheckBagItems.jsx
+++ src/components/CheckBagItems.jsx
@@ -1,44 +1,7 @@
-// import React from 'react'; 
-// import { useSelector , useDispatch} from 'react-redux';
-// import Rating from './Rating';
-// import { incrementQuantity, decrementQuantity } from '../redux/actions/actions';
-
-// const CheckBagItems = ({productId}) => {
-//     const cartItems = useSelector(state => state.cart.products);
-
-//     const dispatch = useDispatch();
-
-//   const handleIncrement = () => {
-//     dispatch(incrementQuantity(productId));
-//   };
-
-//   const handleDecrement = () => {
-//     dispatch(decrementQuantity(productId));
-//   };
-//     return(
-//         <div>
-//         {cartItems.map(products => (
-//           <div key={products.id}>
-//           <img src={products.image} alt={products.name} />
-//             {products.name}
-//             {products.smallDescription}
-//             {products.description}
-//             <Rating stars={products.rating}/>
-//             <span>${products.price} x {products.quantity}</span>
-//             <button onClick={handleDecrement}>-</button>
-//         {products.quantity}
-//         <button onClick={handleIncrement}>+</button>
-//           </div>
-//         ))}
-//       </div>
-//     )
-// }
-
-// export default CheckBagItems; 
-
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increaseQuantity, decreaseQuantity } from '../redux/actions/actions';
+import Rating from '../components/Rating'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CheckBagItems = () => {
@@ -67,6 +30,7 @@ const CheckBagItems = () => {
                             <h5>{products.name}</h5>
                             <p>{products.smallDescription}</p>
                             <p>{products.description}</p>
+                            <Rating stars={products.rating}/> 
                             <p>${products.price}</p>
                         </div>
                         <div className="col-md-3">

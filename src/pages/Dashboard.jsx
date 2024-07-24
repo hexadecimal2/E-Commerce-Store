324a929100443b55eb8795aed40dc3deb9c48275
MbaliPuso <mbalipuso@gmail.com>
2024-07-24 11:40:15 +0200

    added sidebar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProductList from '../components/ProductList';
 import Sidebag from '../components/Sidebag';
+import Sidebar from '../components/Sidebar';
 
 const Dashboard = () => {
     return (
         <>
+        <Sidebar />
         <ProductList />
         <Sidebag />
         </>

21eb88645e82f708f6b56b2eb96db2fbaa95cac2
hexadecimal2 <kidlemon67@gmail.com>
2024-07-24 17:11:03 +0200

    item-view styled

--- src/pages/ItemView.jsx
+++ src/pages/ItemView.jsx
@@ -11,15 +11,15 @@ const ItemView = () => {
 
         <Row>
 
-            <Col md={3} style={{border: '1px solid green'}}>
+            <Col md={2}>
             
             </Col>
 
-            <Col md={6} style={{border: '1px solid green'}}>
+            <Col md={7}>
             <ItemDisplay/>
             </Col>
 
-            <Col md={3} style={{border: '1px solid green'}}>
+            <Col md={3}>
             <Sidebag/>
             </Col>
 

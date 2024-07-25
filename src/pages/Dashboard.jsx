9a8f3e9bc9d71a7cd7f5eb33ca9471e14bafb29a
MbaliPuso <mbalipuso@gmail.com>
2024-07-25 10:57:41 +0200

    Bag fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
                 </div>
                 <div className="col-3 me-0">
                 <Sidebag caption="  View Bag" link = "/check-bag"/>
-                <Sidebag caption = "View Bag" link='/check-bag'/>
+                
                 </div>
             </div>
         </div>
